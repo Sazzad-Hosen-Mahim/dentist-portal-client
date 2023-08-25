cbf9a45ed45924aec6511a191fc0b37eada12e70
Sazzad Hosen <108649437+Sazzad-Hosen-Mahim@users.noreply.github.com>
2023-08-25 11:13:52 +0600

    login page updated\

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  const { register, handleSubmit } = useForm();
+
+  const handleLogin = (data) => {
+    console.log(data);
+  };
   return (
-    <div>
-      <h2>This is login</h2>
+    <div className="h-[800px] flex justify-center items-center">
+      <div className="w-96 p-8">
+        <h2 className="text-xl font-bold text-center">Login</h2>
+        <form onSubmit={handleSubmit(handleLogin)}>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              {...register("email")}
+              type="text"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              {...register("password")}
+              type="password"
+              className="input input-bordered w-full max-w-xs"
+            />
+            <label className="label">
+              <span className="label-text">Forger Password?</span>
+            </label>
+          </div>
+
+          <input
+            className="btn btn-accent w-full"
+            value="Login"
+            type="submit"
+          />
+        </form>
+        <p className="mt-3">
+          New to Dentist Portal?{" "}
+          <Link className="text-secondary" to="/signup">
+            Create New Account!
+          </Link>
+        </p>
+        <div className="divider">OR</div>
+        <button className="btn btn-outline w-full">Continue With Google</button>
+      </div>
     </div>
   );
 };
