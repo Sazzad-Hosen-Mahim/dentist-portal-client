e862940ea5de5290e5d92e593d6bcb5dbed702ad
Sazzad Hosen <108649437+Sazzad-Hosen-Mahim@users.noreply.github.com>
2023-08-26 18:31:33 +0600

    login and signup regex

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const Login = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
 
   const handleLogin = (data) => {
     console.log(data);
@@ -18,20 +22,37 @@ const Login = () => {
               <span className="label-text">Email</span>
             </label>
             <input
-              {...register("email")}
+              {...register("email", { required: "Email Address is required" })}
               type="text"
               className="input input-bordered w-full max-w-xs"
             />
+            {errors.email && (
+              <p role="alert" className="text-red-600">
+                {errors.email?.message}
+              </p>
+            )}
           </div>
           <div className="form-control w-full max-w-xs">
             <label className="label">
               <span className="label-text">Password</span>
             </label>
             <input
-              {...register("password")}
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 5,
+                  message: "Password must be at least 6 characters long",
+                },
+              })}
               type="password"
               className="input input-bordered w-full max-w-xs"
             />
+            {errors.password && (
+              <p role="alert" className="text-red-600">
+                {errors.password?.message}
+              </p>
+            )}
+
             <label className="label">
               <span className="label-text">Forger Password?</span>
             </label>
---
+++ src/Pages/SignUp/SignUp.js
@@ -0,0 +1,90 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+
+const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handleSignUp = (data) => {
+    console.log(data);
+  };
+  return (
+    <div className="h-[800px] flex justify-center items-center">
+      <div className="w-96 p-8">
+        <h2 className="text-xl font-bold text-center">Sign Up</h2>
+        <form onSubmit={handleSubmit(handleSignUp)}>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              className="input input-bordered w-full max-w-xs"
+              {...register("name", { required: "Name is required" })}
+            />
+            {errors.name && (
+              <p className="text-red-600">{errors.name.message}</p>
+            )}
+          </div>
+
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              className="input input-bordered w-full max-w-xs"
+              {...register("email", { required: true })}
+            />
+            {errors.email && (
+              <p className="text-red-600">{errors.email.message}</p>
+            )}
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              className="input input-bordered w-full max-w-xs"
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 6,
+                  message: "Password must be 6 characters long",
+                },
+                pattern: {
+                  value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/,
+                  message: "Password Must be strong",
+                },
+              })}
+            />
+            {errors.password && (
+              <p className="text-red-600">{errors.password.message}</p>
+            )}
+          </div>
+
+          <input
+            className="btn btn-accent w-full mt-5"
+            value="Login"
+            type="submit"
+          />
+        </form>
+        <p className="mt-3">
+          Already have an account?{" "}
+          <Link className="text-secondary" to="/login">
+            Please Login!
+          </Link>
+        </p>
+        <div className="divider">OR</div>
+        <button className="btn btn-outline w-full">Continue With Google</button>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
